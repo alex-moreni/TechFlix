d13224c969606d1127b4b8e1cbf2f23c2e00d105
alex-moreni <alex.moreni47@gmail.com>
2024-04-16 14:06:35 -0300

    chore(backend): MongoDB setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,8 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -21,4 +23,19 @@ const server = http.createServer(app);
 
 server.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
+});
+
+//Connect to MongoDB
+dotenv.config();
+const MONGODB_URL = process.env.MONGODB_URL || "";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URL);
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongo instance");
+});
+
+mongoose.connection.on("error", (error: Error) => {
+  console.log("Error connecting to mongo:", error);
 });
