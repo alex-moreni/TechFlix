1b17af762f6aa8c3541b426f7eaed82134fc88f1
alex-moreni <alex.moreni47@gmail.com>
2024-04-18 15:05:03 -0300

    feat(backend): Implement login function

--- backend/src/controllers/authentication.ts
+++ backend/src/controllers/authentication.ts
@@ -2,6 +2,47 @@ import express from "express";
 import { random, auth } from "../helpers/crypto";
 import { getUserByEmail, createUser } from "../models/user";
 
+export const login = async (req: express.Request, res: express.Response) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ Error: "Check if the request body is valid." });
+    }
+
+    const user = await getUserByEmail(email).select(
+      "+authentication.salt +authentication.password"
+    );
+
+    if (!user) {
+      return res.status(400).json({ Error: "This user does not exist." });
+    }
+
+    const expectedHash = auth(password, user.authentication.salt);
+
+    if (user.authentication.password != expectedHash) {
+      return res.status(403).json({ Error: "Incorrect password." });
+    }
+
+    const salt = random();
+
+    user.authentication.sessionToken = auth(salt, user._id.toString());
+
+    await user.save();
+
+    res.cookie("TECHFLIX-AUTH", user.authentication.sessionToken, {
+      domain: "localhost",
+      path: "/",
+    });
+    return res.status(200).json(user).end();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
 export const register = async (req: express.Request, res: express.Response) => {
   try {
     const { username, email, password } = req.body;
